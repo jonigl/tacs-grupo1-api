0b486e071a9b481fcf0a5b31f4d2c6d1f72c0a12
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-09-02 20:33:06 -0300

    Lists resource updated. MockupResponse updated. Events resource added.

--- src/main/java/ar/com/tacsutn/grupo1/eventapp/web/ListsController.java
+++ src/main/java/ar/com/tacsutn/grupo1/eventapp/web/ListsController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1")
 public class ListsController {
 
-    @GetMapping("/lists/{id}")
-    public MockupResponse get(@PathVariable Long id) {
-        return new MockupResponse(String.format("List %s",id));
+    @GetMapping("/lists/{list_id}")
+    public MockupResponse get(@PathVariable Long list_id) {
+        return new MockupResponse(String.format("List %s",list_id));
     }
 
     @PostMapping("/lists")
@@ -17,14 +17,29 @@ public class ListsController {
         return new MockupResponse(String.format("List created"));
     }
 
-    @PutMapping("/lists/{id}")
-    public MockupResponse update(@PathVariable Long id) {
-        return new MockupResponse(String.format("List %s updated",id));
+    @PutMapping("/lists/{list_id}")
+    public MockupResponse update(@PathVariable Long list_id) {
+        return new MockupResponse(String.format("List %s updated",list_id));
     }
 
-    @DeleteMapping("/lists/{id}")
-    public MockupResponse delete(@PathVariable Long id) {
-        return new MockupResponse(String.format("List %s deleted",id));
+    @DeleteMapping("/lists/{list_id}")
+    public MockupResponse delete(@PathVariable Long list_id) {
+        return new MockupResponse(String.format("List %s deleted",list_id));
+    }
+
+    @GetMapping("/lists/{list_id}/events")
+    public MockupResponse getEvents(@PathVariable Long list_id) {
+        return new MockupResponse(String.format("Events from list %s",list_id));
+    }
+
+    @PostMapping("/lists/{list_id}/events")
+    public MockupResponse addEvent(@PathVariable Long list_id) {
+        return new MockupResponse(String.format("Event added to list %s",list_id));
+    }
+
+    @DeleteMapping("/lists/{list_id}/events/{event_id}")
+    public MockupResponse deleteEvent(@PathVariable Long list_id, @PathVariable Long event_id) {
+        return new MockupResponse(String.format("Event %s deleted from list %s",event_id, list_id));
     }
 
 }
