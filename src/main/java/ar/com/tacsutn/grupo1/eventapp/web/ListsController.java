f747ce17a1926a0e7178db9aab0b5006497a5780
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-09-02 21:46:48 -0300

    %s changed to %d (Code Review).

--- src/main/java/ar/com/tacsutn/grupo1/eventapp/web/ListsController.java
+++ src/main/java/ar/com/tacsutn/grupo1/eventapp/web/ListsController.java
@@ -9,7 +9,7 @@ public class ListsController {
 
     @GetMapping("/lists/{list_id}")
     public MockupResponse get(@PathVariable Long list_id) {
-        return new MockupResponse(String.format("List %s",list_id));
+        return new MockupResponse(String.format("List %d",list_id));
     }
 
     @PostMapping("/lists")
@@ -19,27 +19,27 @@ public class ListsController {
 
     @PutMapping("/lists/{list_id}")
     public MockupResponse update(@PathVariable Long list_id) {
-        return new MockupResponse(String.format("List %s updated",list_id));
+        return new MockupResponse(String.format("List %d updated",list_id));
     }
 
     @DeleteMapping("/lists/{list_id}")
     public MockupResponse delete(@PathVariable Long list_id) {
-        return new MockupResponse(String.format("List %s deleted",list_id));
+        return new MockupResponse(String.format("List %d deleted",list_id));
     }
 
     @GetMapping("/lists/{list_id}/events")
     public MockupResponse getEvents(@PathVariable Long list_id) {
-        return new MockupResponse(String.format("Events from list %s",list_id));
+        return new MockupResponse(String.format("Events from list %d",list_id));
     }
 
     @PostMapping("/lists/{list_id}/events")
     public MockupResponse addEvent(@PathVariable Long list_id) {
-        return new MockupResponse(String.format("Event added to list %s",list_id));
+        return new MockupResponse(String.format("Event added to list %d",list_id));
     }
 
     @DeleteMapping("/lists/{list_id}/events/{event_id}")
     public MockupResponse deleteEvent(@PathVariable Long list_id, @PathVariable Long event_id) {
-        return new MockupResponse(String.format("Event %s deleted from list %s",event_id, list_id));
+        return new MockupResponse(String.format("Event %d deleted from list %d",event_id, list_id));
     }
 
 }
