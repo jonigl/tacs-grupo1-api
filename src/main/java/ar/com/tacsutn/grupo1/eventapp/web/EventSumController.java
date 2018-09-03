b32c3eaf7047832eece00372504f18a55c12e125
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-09-02 23:58:34 -0300

    Added getV2 method to EventSumController

--- src/main/java/ar/com/tacsutn/grupo1/eventapp/web/EventSumController.java
+++ src/main/java/ar/com/tacsutn/grupo1/eventapp/web/EventSumController.java
@@ -7,8 +7,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EventSumController implements Controller {
-    @GetMapping("/events/total")
+    @GetMapping("/events/total_events")
     public MockupResponse getV1(@RequestParam(value = "from", defaultValue = "01/01/1970") String fromDate,
+                                @RequestParam(value = "to", defaultValue = "31/12/9999") String toDate) {
+
+        // TODO: Add logic
+
+        return new MockupResponse("Se registraron 100 eventos desde la fecha " + fromDate + " hasta " + toDate);
+    }
+
+    @GetMapping("/events")
+    public MockupResponse getV2(@RequestParam(value = "from", defaultValue = "01/01/1970") String fromDate,
                                 @RequestParam(value = "to", defaultValue = "31/12/9999") String toDate) {
 
         // TODO: Add logic
--- src/test/java/ar/com/tacsutn/grupo1/eventapp/web/EventSumControllerTest.java
+++ src/test/java/ar/com/tacsutn/grupo1/eventapp/web/EventSumControllerTest.java
@@ -14,16 +14,26 @@ public class EventSumControllerTest extends ControllerTest {
 
     @Test
     public void v1CorrectURIShouldReturnOkStatus() throws Exception {
-        test200Response("events/total");
+        test200Response("events/total_events");
     }
 
     @Test
     public void v1ShouldReturnDefaultValueWhenNoParamIsSet() throws Exception {
-        testRoute("events/total","Se registraron 100 eventos desde la fecha 01/01/1970 hasta 31/12/9999");
+        testRoute("events/total_events","Se registraron 100 eventos desde la fecha 01/01/1970 hasta 31/12/9999");
     }
 
     @Test
     public void v1ShouldReturnSetValueWhenParamsAreSet() throws Exception {
-        testRoute("events/total?from=14/12/1492&to=02/09/2018","Se registraron 100 eventos desde la fecha 14/12/1492 hasta 02/09/2018");
+        testRoute("events/total_events?from=14/12/1492&to=02/09/2018","Se registraron 100 eventos desde la fecha 14/12/1492 hasta 02/09/2018");
+    }
+
+    @Test
+    public void v2ShouldReturnDefaultValueWhenNoParamIsSet() throws Exception {
+        testRoute("events","Se registraron 100 eventos desde la fecha 01/01/1970 hasta 31/12/9999");
+    }
+
+    @Test
+    public void v2ShouldReturnSetValueWhenParamsAreSet() throws Exception {
+        testRoute("events?from=14/12/1492&to=02/09/2018","Se registraron 100 eventos desde la fecha 14/12/1492 hasta 02/09/2018");
     }
 }
