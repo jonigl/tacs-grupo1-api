692a6a4448864eebfa1a292304ddb75b11db634b
Fernando <wzfernandochen@gmail.com>
2018-09-06 19:08:32 -0300

    Event model (incomplete).

---
+++ src/main/java/ar/com/tacsutn/grupo1/eventapp/domain/Event.java
@@ -0,0 +1,49 @@
+package ar.com.tacsutn.grupo1.eventapp.domain;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel
+public class Event {
+
+    @ApiModelProperty(example = "12345abc")
+    private String id;
+
+    @ApiModelProperty(example = "sample event")
+    private String name;
+
+    @ApiModelProperty(example = "sample event description")
+    private String description;
+
+    // TODO: Add more fields.
+
+    public Event(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
