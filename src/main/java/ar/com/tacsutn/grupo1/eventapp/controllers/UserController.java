b7e4a0b83b122f4ea560760aceae389c46d56491
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-09-18 12:50:45 -0300

    Returns the total number of listEvents by user. The administrator account is required.

--- src/main/java/ar/com/tacsutn/grupo1/eventapp/controllers/UserController.java
+++ src/main/java/ar/com/tacsutn/grupo1/eventapp/controllers/UserController.java
@@ -1,6 +1,7 @@
 package ar.com.tacsutn.grupo1.eventapp.controllers;
 
 import ar.com.tacsutn.grupo1.eventapp.models.User;
+import ar.com.tacsutn.grupo1.eventapp.services.EventListService;
 import ar.com.tacsutn.grupo1.eventapp.services.UserService;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,16 +9,21 @@ import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1")
 @Api(tags = "Users", description = "user resources")
 public class UserController {
 
     private final UserService userService;
+    private final EventListService eventListService;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, EventListService eventListService) {
         this.userService = userService;
+        this.eventListService = eventListService;
     }
 
     /**
@@ -40,5 +46,21 @@ public class UserController {
     @PreAuthorize("hasRole('ADMIN')")
     public User getUser(@PathVariable("user_id") Long userId) {
         return userService.getById(userId).orElseThrow(()-> new ResourceNotFoundException("User id not found"));
+    }
+
+
+    /**
+     * Returns the total number of listEvents by user.
+     * The administrator account is required.
+     *
+     * @param userId requested user's identifier.
+     * @return the requested user.
+     */
+    @GetMapping("/users/{user_id}/total_events")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Map<String,Integer> getTotalOfListEvents(@PathVariable("user_id") Long userId) {
+        Map<String,Integer> response = new HashMap<>();
+        response.put("total_lists", eventListService.getTotalEventListByUserId(userId));
+        return response;
     }
 }
