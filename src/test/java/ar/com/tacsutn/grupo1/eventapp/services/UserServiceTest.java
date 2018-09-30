0f02c1663f90ca0cb75e266f509186784d777007
Fernando <wzfernandochen@gmail.com>
2018-09-30 20:41:55 -0300

    Indent with 4 spaces.

--- src/test/java/ar/com/tacsutn/grupo1/eventapp/services/UserServiceTest.java
+++ src/test/java/ar/com/tacsutn/grupo1/eventapp/services/UserServiceTest.java
@@ -18,54 +18,55 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 @Transactional
 public class UserServiceTest {
-  @Autowired
-  private UserService userService;
-  private User user1, user2;
 
-  @Before
-  public void before() {
-    createUsers();
-  }
+    @Autowired
+    private UserService userService;
+    private User user1, user2;
 
-  @Test
-  public void canCreateUser() {
-      User user3 = new User("JaneDoemann", "1234", "Jane", "Doemann", "jane.doemann@test.com", true, new Date(), null);
-      userService.create(user3);
-      User result = userService.getById(user3.getId()).orElseThrow(NoSuchElementException::new);
-      assertEquals(result.getId(), user3.getId());
-  }
+    @Before
+    public void before() {
+        createUsers();
+    }
 
-  @Test
-  public void canGetUserById() {
-    User result = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
-    assertEquals(result.getId(), user1.getId());
-  }
+    @Test
+    public void canCreateUser() {
+        User user3 = new User("JaneDoemann", "1234", "Jane", "Doemann", "jane.doemann@test.com", true, new Date(), null);
+        userService.create(user3);
+        User result = userService.getById(user3.getId()).orElseThrow(NoSuchElementException::new);
+        assertEquals(result.getId(), user3.getId());
+    }
 
-  @Test
-  public void canAccessPropertiesOfUser() {
-    User result = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
-    assertEquals(result.getLastAccess(), user1.getLastAccess());
-  }
+    @Test
+    public void canGetUserById() {
+        User result = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
+        assertEquals(result.getId(), user1.getId());
+    }
 
-  @Test(expected = NoSuchElementException.class)
-  public void getExceptionWhenUserIsNotFound() {
-    userService.getById(user1.getId() + 123).orElseThrow(NoSuchElementException::new);
-  }
+    @Test
+    public void canAccessPropertiesOfUser() {
+        User result = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
+        assertEquals(result.getLastAccess(), user1.getLastAccess());
+    }
 
-  @Test
-  public void canUpdateUser() {
-      User result = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
-      assertEquals(result.getFirstname(), user1.getFirstname());
-      result.setFirstname("Johnny");
-      userService.save(result);
-      User resultReload = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
-      assertEquals(resultReload.getFirstname(), "Johnny");
-  }
+    @Test(expected = NoSuchElementException.class)
+    public void getExceptionWhenUserIsNotFound() {
+        userService.getById(user1.getId() + 123).orElseThrow(NoSuchElementException::new);
+    }
 
-  private void createUsers() {
-    user1 = new User("JohnDoemann", "1234", "John", "Doemann", "john.doemann@test.com", true, new Date(), null);
-    user2 = new User("JohnDoemann2", "1234", "John", "Doemann", "john.doemann2@test.com", true, new Date(), null);
-    userService.create(user1);
-    userService.createAdmin(user2);
-  }
+    @Test
+    public void canUpdateUser() {
+        User result = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
+        assertEquals(result.getFirstname(), user1.getFirstname());
+        result.setFirstname("Johnny");
+        userService.save(result);
+        User resultReload = userService.getById(user1.getId()).orElseThrow(NoSuchElementException::new);
+        assertEquals(resultReload.getFirstname(), "Johnny");
+    }
+
+    private void createUsers() {
+        user1 = new User("JohnDoemann", "1234", "John", "Doemann", "john.doemann@test.com", true, new Date(), null);
+        user2 = new User("JohnDoemann2", "1234", "John", "Doemann", "john.doemann2@test.com", true, new Date(), null);
+        userService.create(user1);
+        userService.createAdmin(user2);
+    }
 }
