631b9a684b8645bdea3396199431b8a0ccbdccbc
santiago bose <alfredosantiagobose@gmail.com>
2018-10-06 23:06:27 -0300

    primeros tests de AlarmsController

--- src/test/java/ar/com/tacsutn/grupo1/eventapp/controllers/ControllerTest.java
+++ src/test/java/ar/com/tacsutn/grupo1/eventapp/controllers/ControllerTest.java
@@ -1,13 +1,12 @@
 package ar.com.tacsutn.grupo1.eventapp.controllers;
 
 import ar.com.tacsutn.grupo1.eventapp.BootstrapData;
+import ar.com.tacsutn.grupo1.eventapp.client.EventFilter;
+import ar.com.tacsutn.grupo1.eventapp.models.Alarm;
 import ar.com.tacsutn.grupo1.eventapp.models.EventId;
 import ar.com.tacsutn.grupo1.eventapp.models.EventList;
 import ar.com.tacsutn.grupo1.eventapp.models.User;
-import ar.com.tacsutn.grupo1.eventapp.services.EventListService;
-import ar.com.tacsutn.grupo1.eventapp.services.EventService;
-import ar.com.tacsutn.grupo1.eventapp.services.SessionService;
-import ar.com.tacsutn.grupo1.eventapp.services.UserService;
+import ar.com.tacsutn.grupo1.eventapp.services.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -50,6 +50,12 @@ public abstract class ControllerTest {
     @Autowired
     private EventListService listService;
 
+    @Autowired
+    private AlarmService alarmService;
+
+    private Alarm alarm1 , alarm2, alarm3;
+
+
     @MockBean
     private SessionService sessionService;
 
@@ -64,6 +70,33 @@ public abstract class ControllerTest {
 
         userService.save(user1);
         userService.save(user2);
+    }
+
+    private void setAlarms() {
+
+        EventFilter filter1 = new EventFilter()
+                .setKeyword("nodejs")
+                .setStartDateFrom(LocalDateTime.of(2018,10,2,0,0))
+                .setStartDateTo(LocalDateTime.of(2018,10,3,0,0));
+
+        EventFilter filter2 = new EventFilter()
+                .setKeyword("key2")
+                .setStartDateFrom(LocalDateTime.of(2018,1,1,0,0))
+                .setAddress("Palestina123");
+
+        EventFilter filter3 = new EventFilter()
+                .setKeyword("key3")
+                .setStartDateFrom(LocalDateTime.of(2018,10,2,0,0))
+                .setStartDateTo(LocalDateTime.of(2018,10,3,0,0))
+                .setAddress("Plymouth College of Art, Tavistock Place");
+
+        alarm1 = new Alarm(user1,"alarm1",filter1);
+        alarm2 = new Alarm(user2,"alarm2",filter2);
+        alarm3 = new Alarm(user1,"alarm3",filter3);
+
+        alarmService.save(alarm1);
+        alarmService.save(alarm2);
+        alarmService.save(alarm3);
     }
 
     private void setEvents() {
@@ -102,6 +135,7 @@ public abstract class ControllerTest {
         setUsers();
         setEvents();
         setLists();
+        setAlarms();
 
         when(sessionService.getAuthenticatedUser(any(HttpServletRequest.class))).thenReturn(user1);
 
