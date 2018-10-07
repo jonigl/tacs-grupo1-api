7026a3d75371199597d44402819a0079be601237
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-10-07 17:00:17 -0300

    Added Refresh Test

--- src/test/java/ar/com/tacsutn/grupo1/eventapp/controllers/AuthenticationControllerTest.java
+++ src/test/java/ar/com/tacsutn/grupo1/eventapp/controllers/AuthenticationControllerTest.java
@@ -1,12 +1,14 @@
 package ar.com.tacsutn.grupo1.eventapp.controllers;
 
 import ar.com.tacsutn.grupo1.eventapp.BootstrapData;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -17,6 +19,7 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.Filter;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -51,8 +54,7 @@ public class AuthenticationControllerTest {
         mockMvc.perform(
                 post("/api/v1/login")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content("{\"username\": \"user\", \"password\": \"user\"}")
-        )
+                        .content("{\"username\": \"user\", \"password\": \"user\"}"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
@@ -64,8 +66,7 @@ public class AuthenticationControllerTest {
         mockMvc.perform(
                 post("/api/v1/login")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content("{\"username\": \"wrong\", \"password\": \"wrong\"}")
-        )
+                        .content("{\"username\": \"wrong\", \"password\": \"wrong\"}"))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
     }
@@ -77,8 +78,59 @@ public class AuthenticationControllerTest {
         mockMvc.perform(
                 post("/api/v1/login")
                         .contentType(MediaType.APPLICATION_JSON_UTF8)
-                        .content("{\"username\": \"user\", \"password\": \"wrong\"}")
-        )
+                        .content("{\"username\": \"user\", \"password\": \"wrong\"}"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void shouldNotLoginTwice() throws Exception {
+        MockHttpServletResponse loginResponse = mockMvc.perform(
+                post("/api/v1/login")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content("{\"username\": \"user\", \"password\": \"user\"}")).andReturn().getResponse();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String token = objectMapper.readTree(loginResponse.getContentAsString()).get("token").asText();
+
+        mockMvc.perform(
+                post("/api/v1/login")
+                        .header("Authorization", token)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content("{\"username\": \"user\", \"password\": \"user\"}"))
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void canRefresh() throws Exception {
+        MockHttpServletResponse loginResponse = mockMvc.perform(
+                post("/api/v1/login")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content("{\"username\": \"user\", \"password\": \"user\"}")).andReturn().getResponse();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String token = objectMapper.readTree(loginResponse.getContentAsString()).get("token").asText();
+
+        mockMvc.perform(
+                get("/api/v1/refresh")
+                        .header("Authorization", token))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void shouldNotRefreshWhenNotLogged() throws Exception {
+        mockMvc.perform(
+                get("/api/v1/refresh"))
                 .andDo(print())
                 .andExpect(status().isUnauthorized());
     }
