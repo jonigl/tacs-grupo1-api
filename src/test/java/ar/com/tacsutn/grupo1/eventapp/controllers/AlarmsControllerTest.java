06469cc66bb83ea85aa11b9f4d96477b5469fb14
santiluccadeguevara <santiluccadeguevara@est.frba.utn.edu.ar>
2018-10-08 20:28:58 -0300

    Added DELETE, PUT and POST AlarmsController Tests

--- src/test/java/ar/com/tacsutn/grupo1/eventapp/controllers/AlarmsControllerTest.java
+++ src/test/java/ar/com/tacsutn/grupo1/eventapp/controllers/AlarmsControllerTest.java
@@ -6,12 +6,28 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class AlarmsControllerTest extends ControllerTest {
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void canPostAlarm() throws Exception {
+        this.getMockMvc()
+                .perform(post("/api/v1/alarms")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content("{ \"address\": \"argentina\", \"keyword\": \"x\", \"name\": \"x\", \"price\": \"100\", \"startDateFrom\": \"2018-10-08T22:54:28.201Z\", \"startDateTo\": \"2018-10-08T22:54:28.201Z\" }"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
     @WithMockUser(roles = "USER")
     @Transactional
     @DirtiesContext
@@ -30,7 +46,7 @@ public class AlarmsControllerTest extends ControllerTest {
     @Transactional
     @DirtiesContext
     @Test
-    public void shouldNotGetAlarm() throws Exception {
+    public void shouldNotGetAlarmIfNotExists() throws Exception {
         this.getMockMvc()
                 .perform(get("/api/v1/alarms/4"))
                 .andDo(print())
@@ -41,7 +57,100 @@ public class AlarmsControllerTest extends ControllerTest {
     @Transactional
     @DirtiesContext
     @Test
-    public void canGetAlarmSummary() throws Exception {
+    public void canPutAlarm() throws Exception {
+        this.getMockMvc()
+                .perform(put("/api/v1/alarms/1")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content("{ \"address\": \"argentina\", \"keyword\": \"x\", \"name\": \"x\", \"price\": \"100\", \"startDateFrom\": \"2018-10-08T22:54:28.201Z\", \"startDateTo\": \"2018-10-08T22:54:28.201Z\" }"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void shouldNotPutAlarmIfNotExists() throws Exception {
+        this.getMockMvc()
+                .perform(put("/api/v1/alarms/4")
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content("{ \"address\": \"argentina\", \"keyword\": \"x\", \"name\": \"x\", \"price\": \"100\", \"startDateFrom\": \"2018-10-08T22:54:28.201Z\", \"startDateTo\": \"2018-10-08T22:54:28.201Z\" }"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void canDeleteAlarm() throws Exception {
+        this.getMockMvc()
+                .perform(delete("/api/v1/alarms/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void shouldNotDeleteAlarmIfNotExists() throws Exception {
+        this.getMockMvc()
+                .perform(delete("/api/v1/alarms/4"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void canFetchAlarmPagedEvents() throws Exception {
+        this.getMockMvc()
+                .perform(get("/api/v1/alarms/1/fetch"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void canFetchAlarmEvents() throws Exception {
+        this.getMockMvc()
+                .perform(get("/api/v1/alarms/1/fetch?page=1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void shouldNotFetchAlarmIfDoesNotExist() throws Exception {
+        this.getMockMvc()
+                .perform(get("/api/v1/alarms/4/fetch"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void shouldNotFetchAlarmEventsIfPageDoesNotExist() throws Exception {
+        this.getMockMvc()
+                .perform(get("/api/v1/alarms/1/fetch/?page=100000000"))
+                .andDo(print())
+                .andExpect(status().isNotFound());
+
+    }
+
+    @WithMockUser(roles = "USER")
+    @Transactional
+    @DirtiesContext
+    @Test
+    public void canGetTodayAlarms() throws Exception {
         this.getMockMvc()
                 .perform(get("/api/v1/alarms/today"))
                 .andDo(print())
@@ -52,7 +161,7 @@ public class AlarmsControllerTest extends ControllerTest {
     @Transactional
     @DirtiesContext
     @Test
-    public void canGetAlarmSummaryDesc() throws Exception {
+    public void canGetTodayAlarmsSortedDesc() throws Exception {
         this.getMockMvc()
                 .perform(get("/api/v1/alarms/today/?sort=desc"))
                 .andDo(print())
@@ -63,46 +172,10 @@ public class AlarmsControllerTest extends ControllerTest {
     @Transactional
     @DirtiesContext
     @Test
-    public void canGetAlarmSummaryAsc() throws Exception {
+    public void canGetTodayAlarmsSortedAsc() throws Exception {
         this.getMockMvc()
                 .perform(get("/api/v1/alarms/today/?sort=asc"))
                 .andDo(print())
                 .andExpect(status().isOk());
     }
-
-
-    @WithMockUser(roles = "USER")
-    @Transactional
-    @DirtiesContext
-    @Test
-    public void canEventsAlarm() throws Exception {
-        this.getMockMvc()
-                .perform(get("/api/v1/alarms/3/fetch"))
-                .andDo(print())
-                .andExpect(status().isOk());
-    }
-
-    @WithMockUser(roles = "USER")
-    @Transactional
-    @DirtiesContext
-    @Test
-    public void shouldGetEventsWithPage() throws Exception {
-        this.getMockMvc()
-                .perform(get("/api/v1/alarms/1/fetch/?page=1"))
-                .andDo(print())
-                .andExpect(status().isBadRequest());
-    }
-
-    @WithMockUser(roles = "USER")
-    @Transactional
-    @DirtiesContext
-    @Test
-    public void shouldNotGetEventsWithPage() throws Exception {
-        this.getMockMvc()
-                .perform(get("/api/v1/alarms/1/fetch/?page=100000000"))
-                .andDo(print())
-                .andExpect(status().isNotFound());
-
-    }
-
 }
