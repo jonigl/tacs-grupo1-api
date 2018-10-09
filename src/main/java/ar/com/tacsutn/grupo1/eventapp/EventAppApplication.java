fc1388bfad8ca460a39ee5aff59fa49366358dc1
Jonathan Gastón Löwenstern <jonigl@gmail.com>
2018-10-09 18:23:36 -0300

    all origins allowed

--- src/main/java/ar/com/tacsutn/grupo1/eventapp/EventAppApplication.java
+++ src/main/java/ar/com/tacsutn/grupo1/eventapp/EventAppApplication.java
@@ -41,7 +41,7 @@ public class EventAppApplication {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4000");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
